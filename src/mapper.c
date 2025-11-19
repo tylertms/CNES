66ba09f2f73349e6f202868b6e126d5cae8377d2
Tyler Smith <tylertms@icloud.com>
2025-11-19 14:03:41 -0500

    Fix mapper 0

--- src/mapper.c
+++ src/mapper.c
@@ -3,7 +3,7 @@
 uint8_t map_cpu_read_000(_cart* cart, uint16_t addr) {
     uint8_t data = 0x00;
 
-    if (0x6000 <= addr && addr <= 0x7FFF) {
+    if (cart->prg_ram.size && 0x6000 <= addr && addr <= 0x7FFF) {
         uint16_t offset = (addr - 0x6000) & (cart->prg_ram.size - 1);
         data = cart->prg_ram.data[offset];
     } else if (0x8000 <= addr && addr <= 0xFFFF) {
@@ -15,25 +15,26 @@ uint8_t map_cpu_read_000(_cart* cart, uint16_t addr) {
 }
 
 void map_cpu_write_000(_cart* cart, uint16_t addr, uint8_t data) {
-    if (0x6000 <= addr && addr <= 0x7FFF) {
+    if (cart->prg_ram.size && 0x6000 <= addr && addr <= 0x7FFF) {
         uint16_t offset = (addr - 0x6000) & (cart->prg_ram.size - 1);
         cart->prg_ram.data[offset] = data;
-    } else if (0x8000 <= addr && addr <= 0xFFFF) {
-        uint16_t offset = (addr - 0x8000) & (cart->prg_rom.size - 1);
-        cart->prg_rom.data[offset] = data;
     }
 }
 
 uint8_t map_ppu_read_000(_cart* cart, uint16_t addr) {
     uint8_t data = 0x00;
 
-    if (0x0000 <= addr && addr <= 0x1FFF) {
-        data = cart->chr_rom.data[addr];
+    if (cart->chr_ram.size) {
+        data = cart->chr_ram.data[addr & (cart->chr_ram.size - 1)];
+    } else {
+        data = cart->chr_rom.data[addr & (cart->chr_rom.size - 1)];
     }
 
     return data;
 }
 
 void map_ppu_write_000(_cart* cart, uint16_t addr, uint8_t data) {
-    return;
+    if (cart->chr_ram.size) {
+        cart->chr_ram.data[addr & (cart->chr_ram.size - 1)] = data;
+    }
 }
