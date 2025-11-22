8733a2ebab8ae7a0d9df7b7c31e8cda63dc8c19d
Tyler Smith <tylertms@icloud.com>
2025-11-21 21:43:08 -0500

    move dma to ppu

--- src/nes.c
+++ src/nes.c
@@ -1,5 +1,6 @@
 #include "nes.h"
 #include "cpu.h"
+#include "ppu.h"
 #include <string.h>
 
 void nes_init(_nes* nes, _gui* gui) {
@@ -8,7 +9,6 @@ void nes_init(_nes* nes, _gui* gui) {
     nes->cpu.p_ppu = &nes->ppu;
     nes->cpu.p_cart = &nes->cart;
     nes->cpu.p_input = &nes->input;
-    nes->cpu.p_dma = &nes->dma;
     nes->ppu.p_cart = &nes->cart;
     nes->ppu.p_gui = gui;
     nes->ppu.p_cpu = &nes->cpu;
@@ -17,7 +17,7 @@ void nes_init(_nes* nes, _gui* gui) {
 void nes_reset(_nes* nes) {
     cpu_reset(&nes->cpu);
 }
-#include <stdio.h>
+
 void nes_clock(_nes* nes) {
     uint8_t frame_complete = 0;
     while (!frame_complete) {
@@ -26,22 +26,22 @@ void nes_clock(_nes* nes) {
             ppu_clock(&nes->ppu) |
             ppu_clock(&nes->ppu) ;
 
-        if (!nes->dma.is_tranfer) {
+        if (!nes->ppu.dma.is_transfer) {
             cpu_clock(&nes->cpu);
         } else {
-            if (nes->dma.dummy_cycle) {
+            if (nes->ppu.dma.dummy_cycle) {
                 if (nes->master_clock & 1)
-                    nes->dma.dummy_cycle = 0;
+                    nes->ppu.dma.dummy_cycle = 0;
             } else {
                 if (nes->master_clock & 1) {
-                    ((uint8_t*)nes->ppu.oam)[nes->dma.addr++] = nes->dma.data;
+                    ((uint8_t*)nes->ppu.oam)[nes->ppu.dma.addr++] = nes->ppu.dma.data;
 
-                    if (!nes->dma.addr) {
-                        nes->dma.is_tranfer = 0;
-                        nes->dma.dummy_cycle = 1;
+                    if (!nes->ppu.dma.addr) {
+                        nes->ppu.dma.is_transfer = 0;
+                        nes->ppu.dma.dummy_cycle = 1;
                     }
                 } else {
-                    nes->dma.data = cpu_read(&nes->cpu, (nes->dma.page << 8) | nes->dma.addr);
+                    nes->ppu.dma.data = cpu_read(&nes->cpu, (nes->ppu.dma.page << 8) | nes->ppu.dma.addr);
                 }
             }
         }
