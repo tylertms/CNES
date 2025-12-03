9c06b0466678dd945233cb80a91fe9bd43af067c
Tyler Smith <tylertms@icloud.com>
2025-12-03 12:59:46 -0500

    Mapper refactor

--- src/mapper.h
+++ src/mapper.h
@@ -16,26 +16,26 @@
     [num] = {map_init_##num, map_deinit_##num, map_irq_pending_##num, map_cpu_read_##num, map_cpu_write_##num, map_ppu_read_##num, map_ppu_write_##num, NULL}
 
 /* DEFINES */
-MAPDEF(0)
-MAPDEF(1)
-MAPDEF(2)
-MAPDEF(3)
-MAPDEF(4)
-MAPDEF(7)
-MAPDEF(9)
-MAPDEF(79)
+MAPDEF(  0)
+MAPDEF(  1)
+MAPDEF(  2)
+MAPDEF(  3)
+MAPDEF(  4)
+MAPDEF(  7)
+MAPDEF(  9)
+MAPDEF( 79)
 MAPDEF(148)
 
 /* MAPPER TABLE */
 static const _mapper mappers[768] = {
-    MAPPER(0),
-    MAPPER(1),
-    MAPPER(2),
-    MAPPER(3),
-    MAPPER(4),
-    MAPPER(7),
-    MAPPER(9),
-    MAPPER(79),
+    MAPPER(  0),
+    MAPPER(  1),
+    MAPPER(  2),
+    MAPPER(  3),
+    MAPPER(  4),
+    MAPPER(  7),
+    MAPPER(  9),
+    MAPPER( 79),
     MAPPER(148)
 };
 
